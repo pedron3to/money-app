41f5eb489da7f27d344f7134e6e522601d41bc21
pedron3to <62304426+pedron3to@users.noreply.github.com>
2021-07-05 13:53:32 -0300

    added google fonts and app-loading

--- App.tsx
+++ App.tsx
@@ -3,8 +3,26 @@ import { StatusBar } from 'expo-status-bar';
 import { Dashboard } from './src/screens/Dashboard';
 import { ThemeProvider } from 'styled-components/native';
 import theme from './src/global/styles/theme'
+import AppLoading from 'expo-app-loading';
+
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return (
     // <View style={styles.container}>
 
@@ -12,7 +30,7 @@ export default function App() {
       <Dashboard />
       <StatusBar style="auto" />
     </ThemeProvider>
-    // </View>
+    // </View> 
   );
 }
 
