f07f034d4a1077a2d4b08d34624dd87bfba085d6
pedron3to <62304426+pedron3to@users.noreply.github.com>
2021-07-05 14:55:50 -0300

    added header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import { Container, Title } from './styles'
+import {
+  Container,
+  Header,
+  UserContainer,
+  UserInfo,
+  Image,
+  User,
+  UserGreeting,
+  UserName
+
+} from './styles'
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserContainer>
+          <UserInfo>
+            <Image source={{ uri: 'https://avatars.githubusercontent.com/u/62304426?v=4' }} />
+            <User>
+              <UserGreeting>Hello,</UserGreeting>
+              <UserName>Pedro</UserName>
+            </User>
+          </UserInfo>
+        </UserContainer>
+
+      </Header>
     </Container>
   )
 }
